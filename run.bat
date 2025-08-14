79cc94f124ad79c57060292359761aa5bd3245ed
Nikolai <nikolay.kolosovsky@gmail.com>
2025-08-15 00:02:07 +0300

    wip

--- run.bat
+++ run.bat
@@ -1,4 +1,8 @@
-adb connect localhost:21503
+setlocal
+where adb >nul 2>nul && (
+    echo "ADB command found. Attempting to connect..."
+    adb connect localhost:21503
+)
 set "SCRIPT_DIR=%~dp0"
 python "%SCRIPT_DIR%cli.py" auto %*
 echo.
--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-adb connect localhost:21503
+#!/bin/sh
+
+if command -v adb >/dev/null 2>&1; then
+    echo "ADB command found. Attempting to connect..."
+    adb connect localhost:21503
+fi
 script_dir=$(dirname "$0")
 python $script_dir/cli.py auto # --debug
 read 
